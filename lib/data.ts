5e6d7fb8fc28e498ee678ea697f2273566b0dad4
Jupiter Bade <jupiterbade@gmail.com>
2024-09-25 18:12:52 +0545

    added from ofice next.js vercel test

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import primetvImg from "@/public/_primetvhd.png";
+import legalcaseImg from "@/public/legalcase.png";
+import maiwakholaImg from "@/public/maiwakhola.png";
 
 export const links = [
   {
@@ -63,18 +66,21 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: primetvImg,
   },
   {
     title: "rmtDev",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: legalcaseImg,
   },
   {
     title: "Word Analytics",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: maiwakholaImg,
   },
 ] as const;
 
