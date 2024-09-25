e5aec9a9dbff6fe71c800cb3d82932fa9f504c32
Jupiter Bade <jupiterbade@gmail.com>
2024-09-25 18:37:10 +0545

    image

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,6 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import primetvImg from "@/public/_primetvhd.png";
-import legalcaseImg from "@/public/legalcase.png";
-import maiwakholaImg from "@/public/maiwakhola.png";
 
 export const links = [
   {
