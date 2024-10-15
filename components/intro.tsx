b423c1e78c90e726c292253f21162d67c4f88540
Jupiter Bade <jupiterbade@gmail.com>
2024-10-15 14:54:46 +0545

    git added push

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { BsArrowRight } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 import { motion } from "framer-motion";
+import { BsLinkedin } from "react-icons/bs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -66,14 +68,24 @@ export default function Intro() {
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
-          Contact me here <BsArrowRight />
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a className="group bg-white">Download CV</a>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{""}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://linkedin.com"
           target="_blank"
-        ></a>
+        >
+          <BsLinkedin />
+        </a>
       </motion.div>
     </section>
   );
