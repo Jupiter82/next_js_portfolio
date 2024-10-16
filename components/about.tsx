46dcfe7ac3ddf573d4044e652b88fe11089a3591
Jupiter82 <jupiterbade@gmail.com>
2024-10-16 09:41:31 +0545

    added from home chlldren

--- components/about.tsx
+++ components/about.tsx
@@ -10,7 +10,7 @@ export default function About() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
     >
-      <SectionHeading></SectionHeading>
+      <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">
