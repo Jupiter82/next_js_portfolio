065a7db2dd6d7c4e08b5f67c357afdcfb5a83203
Jupiter82 <jupiterbade@gmail.com>
2024-10-21 09:23:12 +0545

    added from home working in project 10-21

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,7 +7,7 @@ export default function Projects() {
   return (
     <section>
       <SectionHeading>My Projects</SectionHeading>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {projectsData.map((items, index) => (
           <React.Fragment key={index}>
             <Project {...items} />
@@ -27,12 +27,12 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
     <div className="group mb-3 sm:mb-8 last:mb-0">
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full ">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          <ul>
+          <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
             {tags.map((items, index) => {
               return (
                 <li
@@ -48,9 +48,20 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
         <Image
           src={imageUrl}
           alt="Projects I worked on"
-          quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition"
+          width={350}
+          height={350}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition
+               group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
+        "
         />
+        {/* className="        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+
+        group-even:right-[initial] group-even:-left-40" */}
       </section>
     </div>
   );
