2a01259a8254bec2cfd158af2c587ed206f677bd
Jupiter Bade <jupiterbade@gmail.com>
2024-11-12 18:05:21 +0545

    push office clsx className and react-intersection-observer

--- components/about.tsx
+++ components/about.tsx
@@ -2,9 +2,21 @@
 import React from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
 export default function About() {
+  const { ref, inView } = useInView();
+
+  const { setActiveSection } = useActiveSectionContext();
+
+  if (inView) {
+    setActiveSection("About");
+  }
+
   return (
     <motion.section
+      ref={ref}
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
---
+++ context/active-section-context.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { createContext, useContext, useState } from "react";
+
+// export default function ActiveSectionContextProvider({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+
+type ActiveSectionContextProviderProps = { children: React.ReactNode };
+type ActiveSectionContextType = {
+  activeSection: string;
+  setActiveSection: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState("Home");
+
+  return (
+    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+//seperate hook
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used with in an ActiveSectionContextProvider"
+    );
+  }
+  return context;
+}
